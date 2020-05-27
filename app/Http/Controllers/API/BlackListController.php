0872f858968756991edc59c28ade22e46e765da2
Jayson M. Pamittan <jayson.pamittan@gmail.com>
2020-05-28 00:01:09 +0800

    Add request validation file.

--- app/Http/Controllers/API/BlackListController.php
+++ app/Http/Controllers/API/BlackListController.php
@@ -3,18 +3,16 @@
 namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\BlacklistRequest;
 use Cashbee\Jobs\ProcessThirdPartyBlacklist;
 use Cashbee\Models\Blacklist;
-
 
 class BlackListController extends Controller
 {
     public function getBlacklist(string $mobileNumber)
     {
         try {
-
             $response = BlackList::whereMobileNumber($mobileNumber);
-
         } catch (\Exception $e) {
             return response()->json(['message' => $e->getMessage()], 422);
         }
@@ -22,10 +20,9 @@ class BlackListController extends Controller
         return $response;
     }
 
-    public function checkBlacklist(Request $request)
+    public function checkBlacklist(BlacklistRequest $request)
     {
         try {
-
             $ProcessThirdPartyBlacklist = new ProcessThirdPartyBlacklist(
                 $request->get('mobile_number'),
                 $request->get('name'),
@@ -36,7 +33,6 @@ class BlackListController extends Controller
                 $request->get('birthdate')
             );
             $response = $ProcessThirdPartyBlacklist->handle();
-
         } catch (\Exception $e) {
             return response()->json(['message' => $e->getMessage()], 422);
         }
---
+++ app/Http/Requests/BlacklistRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BlacklistRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $urlRegex = '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/';
+
+        return [
+            'mobile_number'         => 'required',
+            'name'                  => 'required',
+            'country_code'          => 'required',
+            'identification_type'   => 'required',
+            'identification_number' => 'required',
+            'front_of_id_card'      => 'required|regex:' . $urlRegex,
+            'birthdate'             => 'required'
+        ];
+    }
+}
